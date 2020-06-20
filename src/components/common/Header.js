3ba38131fd370a430bebb552ef7228bced5dc904
ISAAC NYAKOI <isaacnyakoi1@gmail.com>
2020-06-20 10:28:40 +0300

    Add Toaster

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,14 +2,25 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    const activeStyle = {color: "#F15B2A"}
-    return (
-        <nav>
-            <NavLink to="/" activeStyle={activeStyle} exact>Home</NavLink>{" | "}
-            <NavLink to="/courses" activeStyle={activeStyle}>Courses</NavLink>{" | "}
-            <NavLink to="/course" activeStyle={activeStyle}>Course</NavLink>{" | "}
-            <NavLink to="/about" activeStyle={activeStyle}>About</NavLink>
-        </nav>
-    )
-    }
+  const activeStyle = { color: "#F15B2A" };
+  return (
+    <nav>
+      <NavLink to="/" activeStyle={activeStyle} exact>
+        Home
+      </NavLink>
+      {" | "}
+      <NavLink to="/courses" activeStyle={activeStyle}>
+        Courses
+      </NavLink>
+      {" | "}
+      <NavLink to="/course" activeStyle={activeStyle}>
+        Course
+      </NavLink>
+      {" | "}
+      <NavLink to="/about" activeStyle={activeStyle}>
+        About
+      </NavLink>
+    </nav>
+  );
+};
 export default Header;
